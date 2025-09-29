ce1adc49afe8fe1556c1a270747a79d5729837ab
lequa <lequa123@gmail.com>
2025-09-29 21:11:05 +0700

    update function resize

--- Netlify/functions/resize.js
+++ Netlify/functions/resize.js
@@ -1,4 +1,5 @@
 const sharp = require("sharp");
+const fetch = require("node-fetch"); // cần thêm cho netlify
 
 exports.handler = async (event) => {
   try {
@@ -14,17 +15,17 @@ exports.handler = async (event) => {
       };
     }
 
-    // Danh sách link ảnh (phải là link trực tiếp tới file .jpg/.png)
+    // Danh sách link ảnh (cú pháp chuẩn)
     const reiImages = [
-   "https://i.pinimg.com/736x/f6/49/41/f649412a33b9260c2b558297c39d45ac.jpg,
-	https://i.pinimg.com/736x/ec/2a/22/ec2a221b0693b58fa20fca14bbac2358.jpg,
-https://i.pinimg.com/736x/d8/d9/88/d8d98811a0a4565f4038c2fd711036c9.jpg,
-https://i.pinimg.com/736x/19/a8/e1/19a8e1346a197b10baef13271d68ec4a.jpg,
-https://i.pinimg.com/736x/b3/09/51/b309516c7e1287fee304c0021743ea48.jpg,
-https://i.pinimg.com/1200x/d2/5e/5c/d25e5c56f02b05405d75bc0e4e9eafb8.jpg,
-https://i.pinimg.com/736x/ad/82/78/ad82788e06adb0e591335ad650ec806d.jpg,
-https://i.pinimg.com/736x/73/c2/ed/73c2ed6794df3178a049fcfbc4ab3689.jpg,
-https://i.pinimg.com/736x/36/60/c3/3660c3f9ee36da8bc1e18200412b07d6.jpg"
+      "https://i.pinimg.com/736x/f6/49/41/f649412a33b9260c2b558297c39d45ac.jpg",
+      "https://i.pinimg.com/736x/ec/2a/22/ec2a221b0693b58fa20fca14bbac2358.jpg",
+      "https://i.pinimg.com/736x/d8/d9/88/d8d98811a0a4565f4038c2fd711036c9.jpg",
+      "https://i.pinimg.com/736x/19/a8/e1/19a8e1346a197b10baef13271d68ec4a.jpg",
+      "https://i.pinimg.com/736x/b3/09/51/b309516c7e1287fee304c0021743ea48.jpg",
+      "https://i.pinimg.com/1200x/d2/5e/5c/d25e5c56f02b05405d75bc0e4e9eafb8.jpg",
+      "https://i.pinimg.com/736x/ad/82/78/ad82788e06adb0e591335ad650ec806d.jpg",
+      "https://i.pinimg.com/736x/73/c2/ed/73c2ed6794df3178a049fcfbc4ab3689.jpg",
+      "https://i.pinimg.com/736x/36/60/c3/3660c3f9ee36da8bc1e18200412b07d6.jpg"
     ];
 
     // random 1 ảnh
@@ -40,7 +41,7 @@ https://i.pinimg.com/736x/36/60/c3/3660c3f9ee36da8bc1e18200412b07d6.jpg"
     // resize (giữ tỷ lệ, fit inside để không bị crop)
     let pipeline = sharp(inputBuffer).resize(w || null, h || null, { fit: "inside" });
 
-    // Chọn format trả về dựa trên content-type gốc (png -> png, else -> jpeg)
+    // Chọn format trả về dựa trên content-type gốc
     let outBuffer;
     if (contentType.includes("png")) {
       outBuffer = await pipeline.png().toBuffer();
